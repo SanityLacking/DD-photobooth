71bc0c00fcd7f564572709118b677226310d42f6
cailen robertson <cailen.robertson@griffith.edu.au>
2018-08-08 02:49:31 +0000

    update pythontf.js

--- pythonTf.js
+++ pythonTf.js
@@ -1,7 +1,7 @@
 var PythonShell = require('python-shell');
-var pythonTF;
 
-	pythonTF.processImg = function(inputA,checkpointS,callback){
+
+function processImg(inputA,checkpointS,callback){
 		var inputArg = inputA;
 		var imgtype =".jpg";
 		var outputArg = inputArg.substring(0,inputArg.lastIndexOf("."))+"_"+Math.floor(new Date() / 1000)+imgtype;
@@ -44,27 +44,4 @@ var pythonTF;
  			callback(outputArg);
 		});
 	}	
-module.exports = pythonTF;
-
-/*
-
-var express = require('express');
-var router = express.Router();
-var pythonTF = require("../test.js");
-
-
-router.get('/', function(req, res, next) {
-  pythonTF.processImg("chicago.jpg","wave",function(result){
-  	console.log("image processed: "+result);
-  });
-  res.render('index', { title: 'Express' });
-
-});
-
-router.get('/test', function(req, res, next) {
-
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
-*/
+module.exports.processImg = processImg;
