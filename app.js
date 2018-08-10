25ca1fbc127328bf77a8b39e56f6f1112e6585da
Carl Albrecht <carl@areait.com.au>
2018-08-10 12:58:50 +1000

    Frontend boilerplate

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use(express.logger('dev'));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // development only
 if ('development' == app.get('env')) {
