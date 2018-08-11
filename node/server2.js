90e9816b3b68b8db72f8b95b769ae933723c39a3
Ryoma Ohira <r.j.ohira@gmail.com>
2018-08-12 01:13:23 +1000

    Saving images

--- node/server2.js
+++ node/server2.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const multer = require('multer');
  
 const app = express();
 app.use(express.static(__dirname));
@@ -10,15 +11,25 @@ app.use(bodyParser.json({
     limit: '50mb',
     extended: true
 })); // support json encoded bodies
+
+
 // CORS
 const cors = require('cors')
-
 var corsOptions = {
   origin: 'http://localhost:4200',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
-
 app.use(cors(corsOptions))
+
+// Multer
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname+'/uploads');
+      },
+    filename: function (req, file, callback){
+        callback(null, file.fieldname + '-' + Date.now());
+    }
+  });
 
 
 app.use(express.static(__dirname + '../photobooth-client/dist/photobooth-client'));
@@ -28,9 +39,78 @@ app.get('/', function (req, res) {
 
 app.post('/uploadphoto', function(req, res){
     console.log('uploadphoto');
-    image = req.body.image;
-    //console.log(req.body.image)
-    res.send(true);
+    let image = req.body.image;
+
+    // Save base64 image to disk
+    try
+    {
+        // Decoding base-64 image
+        // Source: http://stackoverflow.com/questions/20267939/nodejs-write-base64-image-file
+        function decodeBase64Image(dataString) {
+          var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+          var response = {};
+
+          if (matches.length !== 3) 
+          {
+            return new Error('Invalid input string');
+          }
+
+          response.type = matches[1];
+          response.data = new Buffer(matches[2], 'base64');
+
+          return response;
+        }
+
+        // Regular expression for image type:
+        // This regular image extracts the "jpeg" from "image/jpeg"
+        var imageTypeRegularExpression      = /\/(.*?)$/;      
+
+        // Generate random string
+        var crypto                          = require('crypto');
+        var seed                            = crypto.randomBytes(20);
+        var uniqueSHA1String                = crypto
+                                               .createHash('sha1')
+                                                .update(seed)
+                                                 .digest('hex');
+
+        var base64Data = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAZABkAAD/4Q3zaHR0cDovL25zLmFkb2JlLmN...';
+
+        var imageBuffer                      = decodeBase64Image(base64Data);
+        //var userUploadedFeedMessagesLocation = '../img/upload/feed/';
+        var userUploadedFeedMessagesLocation = 'public/uploads/';
+
+        var uniqueRandomImageName            = 'image-' + uniqueSHA1String;
+        // This variable is actually an array which has 5 values,
+        // The [1] value is the real image extension
+        var imageTypeDetected                = imageBuffer
+                                                .type
+                                                 .match(imageTypeRegularExpression);
+
+        var userUploadedImagePath            = userUploadedFeedMessagesLocation + 
+                                               uniqueRandomImageName +
+                                               '.' + 
+                                               imageTypeDetected[1];
+
+        // Save decoded binary image to disk
+        try
+        {
+        require('fs').writeFile(userUploadedImagePath, imageBuffer.data,  
+                                function() 
+                                {
+                                  console.log('DEBUG - feed:message: Saved to disk image attached by user:', userUploadedImagePath);
+                                });
+        }
+        catch(error)
+        {
+            console.log('ERROR:', error);
+        }
+
+    }
+    catch(error)
+    {
+        console.log('ERROR:', error);
+    }
+    
 });
 
 // HTTP listener
@@ -38,3 +118,4 @@ app.listen(3000, function () {
     console.log('Server is running on port 3000');
 });
 module.exports = app;
+
