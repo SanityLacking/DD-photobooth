72e0b363d4b73766cbd3f936fb485011dec670b0
Carl Albrecht <carl@areait.com.au>
2018-08-11 22:24:38 +1000

    Basic watermarking on results page, kinda hacky

--- photobooth-client/src/app/filter-page/filter-page.component.ts
+++ photobooth-client/src/app/filter-page/filter-page.component.ts
@@ -21,17 +21,15 @@ export class FilterPageComponent implements OnInit {
 
 
   ngOnInit() {
-    //this.image = this.route.snapshot.queryParams['image'];
-    
     this.image = sessionStorage.getItem('imageStorage');
     this.width = Number(sessionStorage.getItem('width'));
     this.height = Number(sessionStorage.getItem('height'));
-    //this.width = this.route.snapshot.queryParams['width'];
-    //this.height = this.route.snapshot.queryParams['height'];
   }
 
+
   discardImage() {
-    this.location.back();
+    // this.location.back();
+    this.router.navigate(['result']);
   }
 
 }
